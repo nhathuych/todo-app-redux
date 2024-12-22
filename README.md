5763173c36e88ad377b414b8132123ae0af7ee24
nhathuych <nhathuych@gmail.com>
2024-12-22 17:53:56 +0700

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# todo-app-redux
+
+## Init project via vite
+```
+npm create vite@latest
+```
+
+## Run this command
+```
+npm run dev
+```
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold text-blue-600">Todo App with Redux</h1>
+    </>
+  )
+}
+
+export default App
